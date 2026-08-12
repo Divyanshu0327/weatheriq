6225f53a93b86b2e3764931a3ada8cd2eb161564
Divyanshu0327 <chitturaj317@gmail.com>
2026-08-13 01:42:59 +0530

    fix: remove conflicting mongodb host/port properties overriding URI

--- src/main/java/com/weatheriq/WeatheriqApplication.java
+++ src/main/java/com/weatheriq/WeatheriqApplication.java
@@ -1,13 +1,10 @@
 package com.weatheriq;
 
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Slf4j
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
@@ -15,17 +12,6 @@ public class WeatheriqApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeatheriqApplication.class, args);
-    }
-
-    @PostConstruct
-    public void debugMongoUri() {
-        String uri = System.getenv("MONGODB_URI");
-        if (uri == null) {
-            log.warn("[DEBUG] MONGODB_URI environment variable is NULL/NOT SET");
-        } else {
-            String masked = uri.replaceAll("://([^:]+):([^@]+)@", "://$1:****@");
-            log.warn("[DEBUG] MONGODB_URI resolved value: {}", masked);
-        }
     }
 
 }
